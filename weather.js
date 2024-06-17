2915ee9986c863fc21d40fdc6a676137ea3b54b3
Atul Arpit <168712517+atularpit13@users.noreply.github.com>
2024-06-17 13:50:40 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,64 @@
+const apikey="b37277c7e452c82c84071fa9bdb7048f";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox=document.querySelector(".search input")
+const searchBtn=document.querySelector(".search  button")
+const weatherIcon=document.querySelector(".weather-Icon")
+
+
+async function checkweather(city){
+    const response=await fetch(apiurl+ city+`&appid=${apikey}`);
+    if (response.status==404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    }
+    else {
+        var data=await response.json();
+
+
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°C";
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+    
+        if(data.weather[0].main=='Clouds')
+            {
+                weatherIcon.src="Imgs/clouds.png"
+    
+        }
+        else if(data.weather[0].main=='Clear'){
+            weatherIcon.src="Imgs/clear.png"
+            
+        }
+    
+        else if(data.weather[0].main=='Rain'){
+            weatherIcon.src="Imgs/rain.png"
+            
+        }
+    
+        else if(data.weather[0].main=='Snow'){
+            weatherIcon.src="Imgs/snow.png"
+            
+        }
+    
+        else if(data.weather[0].main=='Drizzle'){
+            weatherIcon.src="Imgs/drizzle.png"
+            
+        }
+    
+        else if(data.weather[0].main=='Mist'){
+            weatherIcon.src="Imgs/mist.png"
+            
+        }
+        document.querySelector(".weather").style.display='block';
+        document.querySelector(".error").style.display="none";
+
+    }
+
+
+}
+searchBtn.addEventListener('click',()=>{
+    checkweather(searchBox.value);
+})
+
+
+
